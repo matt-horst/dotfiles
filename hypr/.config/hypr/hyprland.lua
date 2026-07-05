91e44dd9f5d18124046f86704eefd61da2d1c9f3
matt <mattahorst@gmail.com>
2026-07-04 22:54:55 -0600

    adds bibata cursor

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -57,7 +57,8 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
 
 -----------------------
 ----- PERMISSIONS -----
