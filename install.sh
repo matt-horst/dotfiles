211a1db99c2baaddd40ba098c692145ace1cc041
matt <mattahorst@gmail.com>
2026-07-05 10:07:16 -0600

    adds aur to installation script

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ sudo pacman -Sy
 echo "Installing Pacman packages..."
 grep -v '^#' pkgs-pacman.txt | xargs sudo pacman -S --needed --noconfirm
 
+if command -v paru >/dev/null 2>&1; then
+    echo "Installing AUR packages..."
+    grep -v '^#' pkgs-aur.txt | xargs paru -S --needed --noconfirm
+fi
+
 if command -v stow >/dev/null 2>&1; then
     echo "Stowing configuration files..."
-    stow .
+    stow *
 fi
 
 echo "Setup complete!"
