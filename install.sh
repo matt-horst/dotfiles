45cf368e7cfd9d9b27fc9f88a845bf43b2c18105
Matt Horst <mattahorst@gmail.com>
2026-07-08 14:23:31 -0600

    updates install script to properly stow pkgs and find available AUR helper

--- install.sh
+++ install.sh
@@ -7,16 +7,33 @@ echo "Updating system databases..."
 sudo pacman -Sy
 
 echo "Installing Pacman packages..."
-grep -v '^#' pkgs-pacman.txt | xargs sudo pacman -S --needed --noconfirm
+grep -v '^#' pkgs-pacman.txt | xargs sudo pacman -S --needed
 
-if command -v paru >/dev/null 2>&1; then
-    echo "Installing AUR packages..."
-    grep -v '^#' pkgs-aur.txt | xargs paru -S --needed --noconfirm
+aur_helpers=(paru yay)
+for helper in "${aur_helpers[@]}"; do
+    echo $helper
+    if command -v $helper >/dev/null 2>&1; then
+        aur_helper=$helper
+        break
+    fi
+done
+
+if [[ -v aur_helper ]]; then
+    echo "Installing AUR packages using ${aur_helper}..."
+    grep -v '^#' pkgs-aur.txt | xargs $aur_helper -S --needed
+else
+    echo "AUR helper not found. Skipping AUR packages..."
 fi
 
 if command -v stow >/dev/null 2>&1; then
     echo "Stowing configuration files..."
-    stow *
+    stow_pkgs=(bash delta eza fzf git hypr nvim ssh starship waybar wofi zellij)
+    for pkg in "${stow_pkgs[@]}"; do
+        echo $pkg
+        stow -v $pkg
+    done
+else
+    echo "Stow command not found. Skipping package configuration..."
 fi
 
 echo "Setup complete!"
