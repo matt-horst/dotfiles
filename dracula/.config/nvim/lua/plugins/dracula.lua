72ce887941bd330eff0bf58e3b78e67b12c72723
Matt Horst <mattahorst@gmail.com>
2026-07-11 16:54:24 -0600

    adds cyberdream colors to waybar

---
+++ dracula/.config/nvim/lua/plugins/dracula.lua
@@ -0,0 +1,9 @@
+return {
+    "mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("dracula").setup({})
+        vim.cmd.colorscheme "dracula"
+    end
+}
